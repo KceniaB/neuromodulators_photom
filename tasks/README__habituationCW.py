d37badc4ffd9cdc0583e56b2fe12b980e1591537
KceniaB <kcenia.bougrova@research.fchampalimaud.org>
2023-05-12 14:40:59 +0100

    added modified tasks HCW

---
+++ tasks/README__habituationCW.py
@@ -0,0 +1,7 @@
+""" 
+KceniaB
+12-May-2023
+Code saved from: https://github.com/int-brain-lab/iblrig/tree/master/pybpod_fixtures/IBL/tasks/_iblrig_tasks_habituationChoiceWorld
+Code modified 
+
+"""
